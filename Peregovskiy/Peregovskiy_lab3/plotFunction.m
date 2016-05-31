2c1dd83fa9637a88aef405cb42bf337bee8f3f74
Govski <peregovskiy@gmail.com>
2016-05-31 21:08:10 +0300

    Deleted files from master

--- Peregovskiy/Peregovskiy_lab3/plotFunction.m
+++
@@ -1,9 +0,0 @@
-function state = plotFunction(options, state, flag, points)
-[unused, i] = min(state.Score);
-genotype = state.Population{i};
-plot(points(:, 1), points(:, 2), 'bo');
-hold on;
-plot(points(genotype, 1), points(genotype, 2));
-hold off
-end
-
