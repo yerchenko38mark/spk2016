442f41fe9a7b4405b1cd411cb4cfed6cfae47c2e
ISem <ihorsemw@gmail.com>
2016-06-02 23:52:03 +0300

    Lab2

---
+++ Semkiv/Semkiv_lab2/fun1.m
@@ -0,0 +1,3 @@
+function y = fun1(x) %функція Швефеля 
+y=418.9829*size(x,2)-x.*sin(sqrt(abs(x)));
+end
---
+++ Semkiv/Semkiv_lab2/fun2.m
@@ -0,0 +1,4 @@
+function y = fun2(x) %функція Де Іонга
+y = abs(mod(x(1),2))+abs(mod(x(2),2));
+end
+
